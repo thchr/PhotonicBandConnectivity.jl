e66d3708ada472b15a29a19e19180438022088e5
Thomas Christensen <tchr@mit.edu>
2021-05-10 10:40:09 -0400

    add a convenience accessor to `topology_from_2T1L_xor_1L` that doesn't require explicitly providing longitudinal mode or pinned irreps data

--- src/topology_as_2T1L_vs_1L_difference.jl
+++ src/topology_as_2T1L_vs_1L_difference.jl
@@ -1,6 +1,6 @@
 """
     topology_from_2T1L_xor_1L(nᵀ⁺ᴸ::Vector{Int}, nᴸ::Vector{Int}, 
-            B_Bℤ_BRS::Union{fmpz_mat, Matrix{Int}, BandRepSet})         -> TopologyKind
+            BRS_B_Bℤ::Union{BandRepSet, Matrix{Int}, fmpz_mat})         -> TopologyKind
 
 Determines whether a transverse symmetry vector (``T``) - defined as the difference of a
 transverse + longitudinal (``T+L``) symmetry vector `nᵀ⁺ᴸ` and a longitudinal (``L``)
@@ -10,7 +10,7 @@ symmetry vector `nᴸ` - is topologically trivial or nontrivial from a symmetry 
 - `nᵀ⁺ᴸ`: can be computed from an index vector `cⁱ` and a compatibility Hilbert basis 
   `sb::SymBasis` by `sum_symbases(sb, cⁱ)`; 
 - `nᴸ = sb[idx¹ᴸ]`: where `idx¹ᴸ` is the 1L-constraints pick in the `sb` basis;
-- `B_Bℤ_BRS`: a matrix representation of the elementary band representations (EBR) basis,
+- `BRS_B_Bℤ`: a matrix representation of the elementary band representations (EBR) basis,
   [typically obtained from `bandreps(sgnum, kwarg...)`]. Must be of type `::BandRepSet`, 
   `::Matrix{Int}`, or `Nemo.fmpz_mat` (in order of decreasing conversion-related overhead).
 
@@ -37,12 +37,12 @@ same goes for the associated topology.
 """
 function topology_from_2T1L_xor_1L(nᵀ⁺ᴸ::Vector{Int}, nᴸ::Vector{Int}, Bℤ::fmpz_mat)
     # check topology of L and T+L symmetry vectors
-    is_nontrivialᴸ   = calc_topology(nᴸ,   Bℤ) == nontrivial
-    is_nontrivialᵀ⁺ᴸ = calc_topology(nᵀ⁺ᴸ, Bℤ) == nontrivial
+    is_nontrivialᴸ   = calc_topology(nᴸ,   Bℤ) == NONTRIVIAL
+    is_nontrivialᵀ⁺ᴸ = calc_topology(nᵀ⁺ᴸ, Bℤ) == NONTRIVIAL
     # infer 2T solution's topology from xor-difference:
     is_nontrivialᵀ = is_nontrivialᴸ ⊻ is_nontrivialᵀ⁺ᴸ
 
-    return is_nontrivialᵀ ? nontrivial : trivial
+    return is_nontrivialᵀ ? NONTRIVIAL : TRIVIAL
 end
 
 # convenience accessors
@@ -61,25 +61,46 @@ end
 """
     topology_from_2T1L_xor_1L(nᵀ::Vector{Int}, nᴸ::Vector{Int}, 
                 m²ᵀ::Vector{Int}, Γidxs::AbstractVector{<:Integer}, 
-                B_Bℤ_BRS::Union{fmpz_mat, Matrix{Int}, BandRepSet})      -> TopologyKind
+                BRS_B_Bℤ::Union{BandRepSet, Matrix{Int}, fmpz_mat})      -> TopologyKind
 
 A wrapper over the equivalently named 3-argument signature method, but taking the _physical_
 (i.e. _without_ singular Γ-irrep content) transverse symmetry vector `nᵀ`, the pinned 2T
 symmetry content `m²ᵀ`, and the Γ indexing vector `Γidx` instead of `nᵀ⁺ᴸ` from which it
-reconstruct the latter.
+reconstructs the latter.
 This can often be a more convenient access point.
 
-In similarity with its 3-argument parent method, its final argument `B_Bℤ_BRS` must be a
+In similarity with its 3-argument parent method, its final argument `BRS_B_Bℤ` must be a
 representation of the elementary band representation (EBR) basis, given as either a
 `::BandRepSet`, a `::Matrix{Int}`, or a `::Nemo.fmpz_mat` matrix.
 """
 function topology_from_2T1L_xor_1L(nᵀ::Vector{Int}, nᴸ::Vector{Int},
                             m²ᵀ::Vector{Int}, Γidxs::AbstractVector{Int}, 
-                            B_Bℤ_BRS::Union{fmpz_mat, Matrix{Int}, BandRepSet})
+                            BRS_B_Bℤ::Union{BandRepSet, Matrix{Int}, fmpz_mat})
 
     nᵀ⁺ᴸ = copy(nᵀ)      # reconstruct T+L solution from nᵀ, nᴸ, and m²ᵀ (note the tricky
     nᵀ⁺ᴸ[Γidxs] .+= m²ᵀ  # broadcasted indexing with Γidxs)
     nᵀ⁺ᴸ .+= nᴸ
 
-    return topology_from_2T1L_xor_1L(nᵀ⁺ᴸ, nᴸ, B_Bℤ_BRS)
+    return topology_from_2T1L_xor_1L(nᵀ⁺ᴸ, nᴸ, BRS_B_Bℤ)
+end
+
+# TODO: doc-string
+function topology_from_2T1L_xor_1L(
+            nᵀ::Vector{Int},
+            sb::SymBasis,
+            lgirs::Vector{LGIrrep{3}}, # Γ-irreps
+            BRS_B_Bℤ::Union{BandRepSet, Matrix{Int}, fmpz_mat})
+
+    sb.compatbasis || error(DomainError(sb, "`sb` must be a basis for {BS}"))
+    sb.spinful     && error(DomainError(sb, "`sb` must be a spinless basis"))
+
+    nΓ¹ᴸ = find_representation¹ᴸ(lgirs)
+    nΓ²ᵀ = find_representation²ᵀ(lgirs)
+    
+    Γidxs    = get_Γidxs(lgirs, sb)
+    ntidxs¹ᴸ = find_symmetry_constrained_bases(sb, nΓ¹ᴸ, Γidxs)
+    pick¹ᴸ   = argmin(fillings(sb)[ntidxs¹ᴸ])
+    nᴸ       = sb[pick¹ᴸ]
+
+    topology_from_2T1L_xor_1L(nᵀ, nᴸ, nΓ²ᵀ, Γidxs, BRS_B_Bℤ)
 end
